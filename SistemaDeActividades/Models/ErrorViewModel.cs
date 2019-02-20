15d4ef51e3b76bbc89de7a15c05373674dee8c9e
jamhir789 <erick.medina@itspanuco.edu.mx>
2019-02-20 14:08:44 -0600

    Agregar archivos de proyecto.

---
+++ SistemaDeActividades/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaDeActividades.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
